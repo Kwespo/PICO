7b9f0de72d4f7df40c7915fcfd7c43c27cb8fee2
Kwespo <73428848+Kwespo@users.noreply.github.com>
2023-10-05 13:01:40 +0100

    ye

--- RangeFinder.py
+++ RangeFinder.py
@@ -14,23 +14,23 @@ echo = Pin(20, Pin.IN)
 
 
 def ultra():
-   trigger.low()
-   utime.sleep_us(2)
-   trigger.high()
-   utime.sleep_us(5)
-   trigger.low()
-   while echo.value() == 0:
-       signaloff = utime.ticks_us()
-   while echo.value() == 1:
-       signalon = utime.ticks_us()
-       
-   timepassed = signalon - signaloff
-   distance = (timepassed * 0.0343) / 2
-   distance = "{:.1f}".format(distance)
-   
-   print(distance + " cm")
-   
-   return str(distance)
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(5)
+    trigger.low()
+    while echo.value() == 0:
+        signaloff = utime.ticks_us()
+    while echo.value() == 1:
+        signalon = utime.ticks_us()
+        
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    distance = "{:.1f}".format(distance)
+    
+    print(distance + " cm")
+    
+    return str(distance)
 
 
 
